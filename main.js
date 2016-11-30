a716d4170a3847ce7f946a2ffc93fff518174689
HeinvanBeers <heinvanbeers@hotmail.com>
2016-11-30 15:06:25 +0100

    Possible to add points by mouse clicks

--- main.js
+++ main.js
@@ -3,15 +3,28 @@ var h = 600;
 
 var svg = d3.select("#view").append("svg").attr("width", w).attr("height", h).style("border", "1px solid black");
 
-var nodes = svg
-	.selectAll("circle")
-	.data(data.nodes)
-	.enter()
-	.append("circle");
+function update() {
+	var nodes = svg
+		.selectAll("circle")
+		.data(data.nodes)
+		.enter()
+		.append("circle");
 
+	var nodeAttr = nodes
+		.attr("cx", function (d) { return d.x; })
+		.attr("cy", function (d) { return d.y; })
+		.attr("r", function (d) { return 2; })
+		.style("fill", function(d) { return "blue"; });
+}
 
-var nodeAttr = nodes
-	.attr("cx", function (d) { return d.x; })
-	.attr("cy", function (d) { return d.y; })
-	.attr("r", function (d) { return 2; })
-	.style("fill", function(d) { return "blue"; });
+update();
+
+svg.on("click", function() {
+	var coords = d3.mouse(this);
+    var newData= {
+		x: Math.round(coords[0]),
+        y: Math.round(coords[1])
+    };
+    data.nodes.push(newData);
+    update();
+});
