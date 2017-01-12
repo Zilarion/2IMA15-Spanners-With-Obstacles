fb8d4ce5c3ab378537e649303ed2e820b1bd2eb9
Ruud Andriessen <r.andriessen@ilysian.com>
2017-01-12 13:07:16 +0100

    Updated gulpfile and packages

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var nunjucks = require('gulp-nunjucks');
 var gutil = require('gulp-util');
 var source = require('vinyl-source-stream');
 var sourcemaps = require('gulp-sourcemaps');
+var server = require('gulp-express');
 
 // Compile js
 gulp.task("js", function () {
@@ -36,7 +37,7 @@ gulp.task('sass', function() {
 gulp.task('html', ['js', 'sass'], function() {
     return gulp.src('src/index.html')
         .pipe(nunjucks.compile())
-        .pipe(inject(gulp.src(['www/**/*.js', 'www/**/*.css'], {read: false}), {relative: true, ignorePath: 'www'}))
+        .pipe(inject(gulp.src(['www/**/*.js', 'www/**/*.css'], {read: false}), {relative: false, ignorePath: 'www'}))
         .pipe(gulp.dest('www/'))
 });
 
@@ -46,4 +47,26 @@ gulp.task('build', ['js', 'html'], function() {
 
 gulp.task('watch', function() {
     gulp.watch('src/**/*.*', ['build']);
+});
+
+gulp.task('server', function () {
+    // Start the server at the beginning of the task 
+    server.run(['app.js']);
+ 
+    // Restart the server when file changes 
+    gulp.watch(['app/**/*.html'], server.notify);
+    gulp.watch(['app/styles/**/*.scss'], ['styles:scss']);
+    //gulp.watch(['{.tmp,app}/styles/**/*.css'], ['styles:css', server.notify]); 
+    //Event object won't pass down to gulp.watch's callback if there's more than one of them. 
+    //So the correct way to use server.notify is as following: 
+    gulp.watch(['{.tmp,app}/styles/**/*.css'], function(event){
+        gulp.run('styles:css');
+        server.notify(event);
+        //pipe support is added for server.notify since v0.1.5, 
+        //see https://github.com/gimm/gulp-express#servernotifyevent 
+    });
+ 
+    gulp.watch(['app/scripts/**/*.js'], ['jshint']);
+    gulp.watch(['app/images/**/*'], server.notify);
+    gulp.watch(['app.js', 'routes/**/*.js'], [server.run]);
 });
