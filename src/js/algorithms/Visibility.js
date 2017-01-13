7de347ffaa43b6a2313d358c0540072aab8d3350
Ruud Andriessen <r.andriessen@ilysian.com>
2017-01-13 13:51:36 +0100

    Work on visibility

--- src/js/algorithms/Visibility.js
+++ src/js/algorithms/Visibility.js
@@ -5,25 +5,11 @@ var RBTree = require('bintrees').RBTree;
 
 var Util = require('../core/Util');
 var Graph = require('../core/Graph');
-
-
+var math = require('mathjs');
 
 class Visibility {
 	static compute(g, obstacle) {
 		return Visibility.sweep(g, obstacle);
-		// return Visibility.greedy(g, obstacle);
-	}
-
-	static linePosOnAngle(start, end, sweepPoint, handlePoint){
-		var dx = (handlePoint.x - sweepPoint.x);
-		var dy = (handlePoint.y - sweepPoint.y);
-		var l = Math.sqrt(dx*dx + dy*dy);
-		var stretch = 1000000;
-		//normalize and stretch
-		dx = sweepPoint.x + dx/l * stretch;
-		dy = sweepPoint.y + dy/l * stretch;
-		return Util.intersect(start.x, start.y, end.x, end.y, 
-						   	   sweepPoint.x, sweepPoint.y, dx, dy, true);
 	}
 
 	static angle(center, point) {
@@ -46,11 +32,30 @@ class Visibility {
     return Math.atan(dy / dx)
 	}
 
-	static segmentDist(segment, center, node) {
-		var pOnSegment = Visibility.linePosOnAngle(segment.source, segment.target, center, node);
-		return Util.distance(pOnSegment, center);
+	// Calculates if a segment intersects with the segment between two points
+	static intersects(segment, sweepPoint, node) {
+		// This should give a point on which the segment and the line between sweepPoint -> node cross
+		var p = math.intersect(
+			[segment.source.x, segment.source.y], 
+			[segment.target.x, segment.target.y], 
+			[sweepPoint.x, sweepPoint.y],
+			[node.x, node.y]
+		);  
+
+		// If p is null, then we know the segments do not cross and hence p is in front of the segment
+		console.log(p, [segment.source.x, segment.source.y], [segment.target.x, segment.target.y], [sweepPoint.x, sweepPoint.y], [node.x,node.y]);
+		return p == null ? p : {x: p[0], y: p[1]};
 	}
 
+	static distanceToSegment(segment, p) {
+		var s1 = segment.source;
+		var s2 = segment.target;
+		var top = math.abs((s2.y - s1.y) * p.x - (s2.x - s1.x) * p.y + s2.x * s1.y - s2.y * s1.x)
+		var bottom = math.sqrt(math.pow(s2.y - s1.y ,2) + math.pow(s2.x - s1.x, 2));
+		return top/bottom;
+	}
+
+	// Runs sweepPoint all points in g given an obstacle
 	static sweep(g, obstacle) {
 		console.log("Sweeping")
 		var graph = new Graph();
@@ -59,107 +64,151 @@ class Visibility {
 
 		for (var i = 0; i < graph.nodes.length; i++){
 			var p = graph.nodes[i]
-			var visible = Visibility.sweepPoint(p, graph, obstacle);
-
-			for (var key in visible) {
-				var visibleP = visible[key];
-				graph.addEdge(p, visibleP);
+			if (!p.isObstacle()) {
+				var visible = Visibility.sweepPoint(p, graph, obstacle);
+				for (var key in visible) {
+					var visibleP = visible[key];
+					graph.addEdge(p, visibleP);
+				}
+				return graph;
 			}
-			return graph;
 		}
 
 		return graph;
 	}
 
+	// Sweeps a single point given a graph and obstacle
 	static sweepPoint(point, graph, obstacle) {
 		// S = obstacle edges
 		// Vs = obstacle nodes
 		// Gs = endpoint nodes of S
 		// Vs = nodes with set of edges Es
 
-		var status = new RBTree(function(n1, n2) {
-			if (n1.id === n2.id) {
-				return 0;
-			}
-			var n1Dist = Util.distance(n1.data.node, point);
-			var n2Dist = Util.distance(n2.data.node, point);
-			return n1Dist - n2Dist;
-		});
-
-		// Initialize event queue
+		// Initialize the status and event queue
 		var events = this.initEvents(point, graph, obstacle);
+		var status = this.initStatus(point, graph, obstacle, events);
 
 		var visible = [];
-		while (events.size > 0) {
-			var event = events.min();
+		for (var key in events) {
+			var event = events[key];
 			Visibility.handleEvent(point, event, status, visible);
-			events.remove(event);
 		}
-		// console.log(visible);
 		return visible;
 	}
 
-	static handleEvent(center, e, status, visible) {
+	// Handles an event e given a sweepPoint for a certain status. Fills up visible array with visible points from center
+	static handleEvent(sweepPoint, e, status, visible) {
 		var eventType = e.event;
 		var node = e.node;
-
+		console.log('-------------')
+		console.log(eventType, node.id);
 		switch(eventType) {
 			case "point":
 				var min = status.min();
 				if(min == null) {
+					console.log("min");
 					visible.push(node);
 				} else {
-					var distToSegment = Visibility.segmentDist(min.data.segment, center, node);
-					var distToNode = Util.distance(node, center);
-
+					var p = Visibility.intersects(min.segment, sweepPoint, node);
+					console.log([min.segment.source.id, min.segment.target.id], [sweepPoint.id,node.id]);
+					if (p == null) {
+						console.log("No intersection, so visible")
+						visible.push(node);						
+					}
+					var distToNode = Util.distance(sweepPoint, node);
+					var distToSegment = Util.distance(sweepPoint, p);
+					
+					console.log(distToSegment, distToNode)
 					if (distToSegment > distToNode) {
+						console.log("Point is in front of segment")
 						visible.push(node);
+					} else {
+						console.log("Segment is closer than the point");
 					}
 				}
 			break;
 			case "start":
-				status.insert({id: node.id, data: {node: node, segment: e.segment}});
+				status.insert({id: e.segment.source.id, segment: e.segment});
 				var min = status.min();
-				if(min == null || min.id == node.id) {
+				if(min == null) {
+					console.log("min == null or id's are equal")
 					visible.push(node);
 				}
 			break;
 			case "end":
-				status.remove({id: e.startNode.id, data: {node: e.startNode}});
+				status.remove({id: e.segment.source.id, segment: e.segment});
 				var min = status.min();
-				if(min == null || min.id == node.id) {
+				if(min == null) {
+					console.log("min == null or id's are equal");
 					visible.push(node)
 				}
 			break;
 		}
 	}
 
-	static initEvents(point, graph, obstacle) {
-		var eventQueue = new RBTree(function(n1, n2) {
-			var e1 = n1.node;
-			var e2 = n2.node;
-			if (e1.id == e2.id) {
+	// Initialize the status given a sweepPoint, graph and the obstacle
+	static initStatus(sweepPoint, graph, obstacle, events) {
+		var status = new RBTree(function(n1, n2) {
+			if (n1.id == n2.id) {
 				return 0;
 			}
-
-			// Sort on angle
-			var e1Angle = Visibility.angle(point, e1);
-			var e2Angle = Visibility.angle(point, e2);
-
-			if (e1Angle == e2Angle) {
-				// If equal, sort on distance to this segment
-				e1Dist = Util.distance(e1, point);
-				e2Dist = Util.distance(e2, point);
-				return (e1Dist - e2Dist);
-			}
-			return (e1Angle - e2Angle);
+			return Visibility.distanceToSegment(n1.segment, sweepPoint) - Visibility.distanceToSegment(n2.segment, sweepPoint);
+			// Set distance from the start/source point
+			// var n1Dist = Util.distance(n1.data.node, sweepPoint);
+			// var n2Dist = Util.distance(n2.data.node, sweepPoint);
+			// if (n1Dist == n2Dist) {
+			// 	// Set distance to be the distance to the target point instead
+			// 	n1Dist = Util.distance(n1.data.segment.target, sweepPoint);
+			// 	n2Dist = Util.distance(n2.data.segment.target, sweepPoint);
+			// }
+			// return n1Dist - n2Dist;
 		});
+
+		// // Fill with all points 180 before our loop
+		// var edges = [];
+		// for (var key in obstacle.edges) {
+		// 	var edge = obstacle.edges[key];
+		// 	// If the start of the edge is within 180 of the start
+		// 	if (Visibility.angle(sweepPoint, edge.source) <= 180) {
+		// 		// And the end isn't in this 180 degrees
+		// 		if (!(Visibility.angle(sweepPoint, edge.target) <= 180)) {
+		// 			// We have to add this one to the status
+		// 			edges.push(edge);
+		// 		}
+		// 	}
+		// }
+
+		// edges.sort(function(e1, e2) {
+		// 	Visibility.angle(sweepPoint, edge.source)
+		// });
+
+		// Go through all events and update only loop already
+		for (var key in events) {
+			var e = events[key];
+			switch(e.event) {
+				case "start":
+					var inserted = status.insert({id: e.segment.source.id, segment: e.segment});
+					// console.log("ins ", [e.segment.source.id, e.segment.target.id], ": ", inserted)
+				break;
+				case "end":
+					var removed = status.remove({id: e.segment.source.id, segment: e.segment});
+					// console.log("rem ", [e.segment.source.id, e.segment.target.id], ": ", removed)
+				break;
+			}
+		}
+
+		return status;
+	}
+
+	// Initializes the event queue given a point, graph and the obstacle
+	static initEvents(point, graph, obstacle) {
+		var eventQueue = [];
 
 		// Insert all points
 		for (var key in graph.nodes) {
 			var node = graph.nodes[key];
-			if (!node.isObstacle()) {
-				eventQueue.insert({event: "point", node: node});
+			if (!node.isObstacle() && point.id != node.id) {
+				eventQueue.push({event: "point", node: node});
 			}
 		}
 
@@ -168,51 +217,37 @@ class Visibility {
 			var edge = obstacle.edges[key];
 			var source = edge.source;
 			var target = edge.target;
-			var endEvent = {event: "end", node: graph.nodes[target.id], startNode: graph.nodes[source.id]};
-			eventQueue.insert(endEvent);
-			eventQueue.insert({event: "start", node: graph.nodes[source.id], segment: edge});
+			// This doesn't work, the start points are the points which we see first! Then we get the end points. :TODO:
+			eventQueue.push({event: "end", node: graph.nodes[target.id], segment: edge});
+			eventQueue.push({event: "start", node: graph.nodes[source.id], segment: edge});
+		}
+
+		eventQueue.sort(function(e1, e2) {
+			var n1 = e1.node;
+			var n2 = e2.node;
+			if (n1.id == n2.id) {
+				return 0;
+			}
+
+			// Sort on angle
+			var e1Angle = Visibility.angle(point, n1);
+			var e2Angle = Visibility.angle(point, n2);
+
+			if (e1Angle == e2Angle) {
+				// If equal, sort on distance to this segment
+				var e1Dist = Util.distance(n1, point);
+				var e2Dist = Util.distance(n2, point);
+				return (e1Dist - e2Dist);
+			}
+			return (e1Angle - e2Angle);
+		});
+
+		for (var key in eventQueue) {
+			var event = eventQueue[key];
 		}
 
 		return eventQueue;
 	}
-
-	// static greedy(g, obstacle){
-	// 	console.log("Computing visibility");
-	// 	var graph = new Graph();
-	// 	graph.copy(obstacle, true);
-	// 	graph.copy(g, false);
-
-	// 	var points = graph.nodes;
-	// 	for (var p = 0; p < points.length; p++){
-	// 		for (var q = p+1; q < points.length; q++){
-	// 			if (!Util.lineIntersectsObstacle(points[p], points[q], obstacle)){
-	// 				var pp = points[p];
-	// 				var qq = points[q];
-	// 				//check for boundary lines, which lie inside but do not intersect the polygon
-	// 				var midpoint = {};
-	// 				midpoint.x = pp.x + (qq.x - pp.x)/2;
-	// 				midpoint.y = pp.y + (qq.y - pp.y)/2;
-	// 				if (!Util.pointInsideObstacle(midpoint.x, midpoint.y, obstacle)){
-	// 					graph.addEdge(points[p], points[q], Util.distance(points[p], points[q]));
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-
-	// 	// :TODO: find a decent solution for adding all edges instead
-	// 	var prev = undefined;
-	// 	for (var key in obstacle.nodes) {
-	// 		var p = obstacle.nodes[key]
-	// 		if (prev) {
-	// 			graph.addEdge(prev, p, Util.distance(prev, p));
-	// 		}
-	// 		prev = p;
-	// 	}
-	// 	graph.addEdge(prev, obstacle.nodes[0], Util.distance(prev, obstacle.nodes[0]));
-	// 	console.log("Done computing visibility");
-
-	// 	return graph;
-	// }
 }
 
 module.exports = Visibility;
