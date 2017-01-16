58b940a1cd2b4c1745cb94522bff4d05633b2447
Ruud Andriessen <r.andriessen@ilysian.com>
2017-01-16 09:03:42 +0100

    Work on wspd obstacle

--- src/js/algorithms/WSPD.js
+++ src/js/algorithms/WSPD.js
@@ -2,11 +2,10 @@
 
 var Util = require('../core/Util');
 var QuadTree = require('../core/Quadtree');
-var shortest = require('./Dijkstra');
-var debug;
+var shortest = require('./Astar');
 
 class WSPD {
-	static calculate(graph, visibilityGraph, settings) {
+	static calculate(graph, vgraph, settings) {
 		// Build quad tree
 		var pointQuad = true;
 		var bounds = {
@@ -15,32 +14,75 @@ class WSPD {
 		    width: settings.bounds.xmax - settings.bounds.xmin,
 		    height: settings.bounds.ymax - settings.bounds.ymin
 		}
-		debug = {circles: [], rects: []};
 		var quad = new QuadTree(bounds, pointQuad, 9999999, 1);
-		quad.insert(graph.nodes);
+		for (var key in graph.nodes) {
+			var node = graph.nodes[key];
+			if (!node.isObstacle()) {		
+				quad.insert(node);
+			}
+		}
 
 		var t = settings.t
 		var s = 4 * (t+1)/(t-1)
 
-		var r = WSPD.wsPairs(quad.root, quad.root, quad, s);
+		var r = WSPD.wsPairs(quad.root, quad.root, quad, s, vgraph);
 		
-		var map = {};
 		for (var key in r) {
 			var pair = r[key];
-
-			debug.circles.push(WSPD.createCircle(pair.u))
-			debug.circles.push(WSPD.createCircle(pair.v))
-
 			var repu = WSPD.rep(pair.u);
 			var repv = WSPD.rep(pair.v);
 
-			// map[repu.id] ? map[repu.id].push(repv.id) : map[repu.id] = [repv.id];
-			// map[repv.id] ? map[repv.id].push(repu.id) : map[repv.id] = [repu.id];
-			// console.log(pair.u, pair.v)
-			graph.addEdge(repu, repv, Util.distance(repu, repv));
+			if (pair.u.children.length == 0 || pair.v.children.length == 0) {
+				for (var k1 in pair.u) {
+					for (var k2 in pair.u) {
+						var n1 = pair.u[k1];
+						var n2 = pair.u[k2];
+						if (n1.id != n2.id) {
+							if (!WSPD.isVisible(n1, n2, vgraph)) {
+								// Two nodes in this set cannot see eachother, find the shortest path in vgraph
+								WSPD.addPathToGraph(shortest.calculate(vgraph.nodes[n1.id], vgraph.nodes[n2.id]), graph);
+							}
+						}
+					}
+				}
+			} else if (pair.u.children.length == 1 && pair.v.children.length == 1) {
+				var n1 = pair.u.children[0];
+				var n2 = pair.v.children[0];
+				if (!WSPD.isVisible(n1, n2, vgraph)) {
+					// console.log(n1.id, n2.id);
+					// Two nodes in this set cannot see eachother, find the shortest path in vgraph
+					WSPD.addPathToGraph(shortest.calculate(vgraph.nodes[n1.id], vgraph.nodes[n2.id]), graph);
+				}
+			} else {
+				graph.addEdge(repu, repv, Util.distance(repu, repv));
+			}
 		}
-		// console.log(map)
 		return graph;
+	}
+
+	static addPathToGraph(path, graph) {
+		var prev = undefined;
+		for (var p in path.sequence){
+			var point = path.sequence[p];
+			if (prev){
+				var source = graph.nodes[prev.id] ? graph.nodes[prev.id] : prev;
+				var target = graph.nodes[point.id] ? graph.nodes[point.id] : point;
+				graph.addEdge(source, target, Util.distance(prev, point));
+			}
+			prev = point;
+		}
+	}
+
+	static isVisible(n1, n2, vgraph) {
+		var u = vgraph.nodes[n1.id];
+		var v = vgraph.nodes[n2.id];
+		for (var key in u.edges) {
+			var neighbor = u.edges[key];
+			if (neighbor.id == v.id) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	// Check if this node is a leaf
@@ -128,7 +170,7 @@ class WSPD {
 	}
 
 	// ws pairs function
-	static wsPairs(u, v, T, s) {
+	static wsPairs(u, v, T, s, vGraph) {
 		if (WSPD.isempty(WSPD.rep(u)) || WSPD.isempty(WSPD.rep(v)) || (WSPD.isLeaf(u) && WSPD.isLeaf(v) && u === v)) {
 			return [];
 		} else if (WSPD.seperated(u, v, s)) {
@@ -139,8 +181,6 @@ class WSPD {
 				v = u;
 				u = temp;
 			}
-			debug.rects.push(u._bounds)
-			debug.rects.push(v._bounds)
 			var childNodes = u.nodes;
 			var result = [];
 			for (var key in childNodes) {
